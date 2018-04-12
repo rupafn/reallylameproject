3177a434b3f785f9768e6c8de9633b8730d3e4f1
Fajilatun Nahar <fajilatun@asia-genomics.com>
2018-04-12 18:25:20 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './include/Header';
+import Login from './views/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Login/>
 
       </div>
     );
---
+++ src/views/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class Login extends Component {
+  render() {
+    return (
+
+        <div className="login-wrapper">
+              <form>
+                
+              </form>
+        </div>
+
+    );
+  }
+}
+
+export default Login;
